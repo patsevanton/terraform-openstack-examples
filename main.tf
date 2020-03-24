a2218bce1f9a27ab646fa5e760da85732c82d98f
Anton Patsev <patsev.anton@gmail.com>
2020-03-24 15:43:27 +0600

    add openstack_compute_instance_v2 test-vm

--- main.tf
+++ main.tf
@@ -2,3 +2,16 @@ resource "openstack_compute_keypair_v2" "my-cloud-key" {
   name       = "my-key"
   public_key = file("/home/user/.ssh/id_rsa.pub")
 }
+
+resource "openstack_compute_instance_v2" "test" {
+  name            = "test-vm"
+  image_name      = "CentOS_7.6_CloudImage_201901"
+  flavor_name     = "c1.m1.d40"
+  key_pair        = "${openstack_compute_keypair_v2.my-cloud-key.name}"
+  security_groups = ["default"]
+
+  network {
+    name = "public"
+  }
+}
+
