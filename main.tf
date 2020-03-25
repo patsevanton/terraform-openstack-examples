bd89287503a52d3af2c918b5700bcfe32573125e
Anton Patsev <patsev.anton@gmail.com>
2020-03-25 14:18:21 +0600

    openstack_compute_keypair_v2.my-cloud-key.name

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "openstack_compute_instance_v2" "test" {
   name            = "test-vm"
   image_name      = "CentOS_7.6_CloudImage_201901"
   flavor_name     = "c1.m1.d40"
-  key_pair        = "${openstack_compute_keypair_v2.my-cloud-key.name}"
+  key_pair        = openstack_compute_keypair_v2.my-cloud-key.name
   security_groups = ["default"]
 
   network {
