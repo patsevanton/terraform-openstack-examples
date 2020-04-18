193c0c901f8ec55645a037bef7cb51252b2d8a36
Anton Patsev <patsev.anton@gmail.com>
2020-04-18 17:31:40 +0600

    refactored run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-terraform init
-terraform apply -auto-approve
+terraform_version='0.12.24'
+
+terraform_archive=terraform_${terraform_version}_linux_amd64.zip
+if [ ! -f "$terraform_archive" ]; then
+    curl https://releases.hashicorp.com/terraform/${terraform_version}/terraform_${terraform_version}_linux_amd64.zip -o "$terraform_archive"
+fi
+
+if [ ! -f terraform ]; then
+    unzip "$terraform_archive"
+fi
+
+./terraform init
+./terraform apply -auto-approve
