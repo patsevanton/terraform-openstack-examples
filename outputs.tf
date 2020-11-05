4a8bdd75ffa3ec12e1c88a064a8d3a79f9b29916
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2020-11-05 13:29:18 +0600

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+resource "local_file" "AnsibleInventory" {
+  content = templatefile("inventory.tmpl",
+    {
+      application1_name                   = yandex_compute_instance.application1.name,
+      application1_external_ip_address    = yandex_compute_instance.application1.network_interface.0.nat_ip_address,
+      application1_internal_ip_address    = yandex_compute_instance.application1.network_interface.0.ip_address,
+      
+      zookeeper_name                = yandex_compute_instance.zookeeper.*.name,
+      zookeeper_external_ip_address = yandex_compute_instance.zookeeper.*.network_interface.0.nat_ip_address,
+      zookeeper_internal_ip_address = yandex_compute_instance.zookeeper.*.network_interface.0.ip_address,
+    }
+  )
+  filename = "inventory.ini"
+}
