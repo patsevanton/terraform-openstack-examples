4df2b1862cebea6dd45ce758569c0b2557eba2cc
Anton Patsev <10828883+patsevanton@users.noreply.github.com>
2020-11-05 13:28:26 +0600

    ansible-galaxy install andrewrothstein.zookeeper --force run.sh

--- run.sh
+++ run.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-terraform_version='0.12.24'
-
-terraform_archive=terraform_${terraform_version}_linux_amd64.zip
-if [ ! -f "$terraform_archive" ]; then
-    curl https://releases.hashicorp.com/terraform/${terraform_version}/terraform_${terraform_version}_linux_amd64.zip -o "$terraform_archive"
-fi
-
-if [ ! -f terraform ]; then
-    unzip "$terraform_archive"
-fi
-
 ./terraform init
 time ./terraform apply -auto-approve
 sleep 60
+
 ansible-playbook disable_selinux.yml
-#ansible-galaxy install andrewrothstein.zookeeper
 ansible-galaxy install andrewrothstein.zookeeper-cluster
+# https://github.com/andrewrothstein/ansible-zookeeper-cluster/issues/9
+ansible-galaxy install andrewrothstein.zookeeper --force
 ansible-playbook install_zookeeper_cluster.yml
 #ansible-galaxy install geerlingguy.gitlab
